231c1a359de47e68a07c53cb7026374aacfdb44e
Manish chandra joshi <joshi.manishchandra@gmail.com>
2026-05-02 00:57:36 +0530

    update

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        products: resolve(__dirname, 'products.html'),
+        productDetail: resolve(__dirname, 'product-detail.html'),
+        commission: resolve(__dirname, 'commission.html'),
+      },
+    },
+  },
+});
